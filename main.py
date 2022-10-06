72d58c0b52cdf0fd583650a5eeda5d945bcc2dbc
ד"ר מיאו <109675030+harelattias@users.noreply.github.com>
2022-10-06 16:14:38 +0200

    added sps

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 def on_gesture_shake():
+    on_start_sps()
     on_start_mg()
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+def on_start_sps():
+    basic.show_leds("""
+        . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                # . # . #
+    """)
+    music.play_melody("- - - C5 B A B C5 ", 120)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+    """)
+    input.set_sound_threshold(SoundThreshold.LOUD, 255)
+    input.set_sound_threshold(SoundThreshold.QUIET, 254)
+    led.set_brightness(128)
 
 def on_button_pressed_a():
     global temporary_chosen_game
@@ -15,6 +37,19 @@ def on_button_pressed_a():
         temporary_chosen_game = 0
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def forever_sps():
+    if input.light_level() < 70:
+        led.set_brightness(50)
+    elif input.light_level() > 205:
+        led.set_brightness(255)
+    else:
+        led.set_brightness(128)
+
+def on_sound_loud():
+    if 0 == chosen_game:
+        sound_loud_on_sps()
+input.on_sound(DetectedSound.LOUD, on_sound_loud)
+
 def on_button_pressed_b():
     global temporary_chosen_game
     if chosen_game == -1:
@@ -27,6 +62,40 @@ def on_button_pressed_b():
         """)
         temporary_chosen_game = 1
 input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def sound_loud_on_sps():
+    global gg_sps
+    basic.show_number(3)
+    basic.pause(500)
+    basic.show_number(2)
+    basic.pause(500)
+    basic.show_number(1)
+    basic.pause(500)
+    gg_sps = randint(1, 3)
+    if gg_sps == 1:
+        basic.show_leds("""
+            . . # . .
+                        . # # # .
+                        # # # # #
+                        . # # # .
+                        . . # . .
+        """)
+    elif gg_sps == 2:
+        basic.show_leds("""
+            # . . . #
+                        . # . # .
+                        . . # . .
+                        # # . # #
+                        # # . # #
+        """)
+    elif gg_sps == 3:
+        basic.show_leds("""
+            # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+        """)
 
 def on_logo_pressed():
     global chosen_game
@@ -44,13 +113,14 @@ def on_start_mg():
     """)
     temporary_chosen_game = -1
     chosen_game = -1
+gg_sps = 0
 temporary_chosen_game = 0
 chosen_game = 0
 on_start_mg()
 
 def on_forever():
     if 0 == chosen_game:
-        basic.show_icon(IconNames.YES)
+        forever_sps()
     if 1 == chosen_game:
-        basic.show_icon(IconNames.YES)
+        pass
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,6 +1,29 @@
 input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    on_start_sps()
     on_start_mg()
 })
+function on_start_sps() {
+    basic.showLeds(`
+        . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                # . # . #
+    `)
+    music.playMelody("- - - C5 B A B C5 ", 120)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+    `)
+    input.setSoundThreshold(SoundThreshold.Loud, 255)
+    input.setSoundThreshold(SoundThreshold.Quiet, 254)
+    led.setBrightness(128)
+}
+
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
     if (chosen_game == -1) {
@@ -12,6 +35,23 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
                         # # . # #
         `)
         temporary_chosen_game = 0
+    }
+    
+})
+function forever_sps() {
+    if (input.lightLevel() < 70) {
+        led.setBrightness(50)
+    } else if (input.lightLevel() > 205) {
+        led.setBrightness(255)
+    } else {
+        led.setBrightness(128)
+    }
+    
+}
+
+input.onSound(DetectedSound.Loud, function on_sound_loud() {
+    if (0 == chosen_game) {
+        sound_loud_on_sps()
     }
     
 })
@@ -29,6 +69,43 @@ input.onButtonPressed(Button.B, function on_button_pressed_b() {
     }
     
 })
+function sound_loud_on_sps() {
+    
+    basic.showNumber(3)
+    basic.pause(500)
+    basic.showNumber(2)
+    basic.pause(500)
+    basic.showNumber(1)
+    basic.pause(500)
+    gg_sps = randint(1, 3)
+    if (gg_sps == 1) {
+        basic.showLeds(`
+            . . # . .
+                        . # # # .
+                        # # # # #
+                        . # # # .
+                        . . # . .
+        `)
+    } else if (gg_sps == 2) {
+        basic.showLeds(`
+            # . . . #
+                        . # . # .
+                        . . # . .
+                        # # . # #
+                        # # . # #
+        `)
+    } else if (gg_sps == 3) {
+        basic.showLeds(`
+            # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+        `)
+    }
+    
+}
+
 input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
     
     chosen_game = temporary_chosen_game
@@ -46,16 +123,17 @@ function on_start_mg() {
     chosen_game = -1
 }
 
+let gg_sps = 0
 let temporary_chosen_game = 0
 let chosen_game = 0
 on_start_mg()
 basic.forever(function on_forever() {
     if (0 == chosen_game) {
-        basic.showIcon(IconNames.Yes)
+        forever_sps()
     }
     
     if (1 == chosen_game) {
-        basic.showIcon(IconNames.Yes)
+        
     }
     
 })
